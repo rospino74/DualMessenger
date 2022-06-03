ab69db6609d539b5ec7fbad2a46cbd0a5a12bd99
Marko <34315725+rospino74@users.noreply.github.com>
2022-06-03 14:44:13 +0200

    Refactored the promise system

--- src/components/APKPicker.svelte
+++ src/components/APKPicker.svelte
@@ -1,22 +1,37 @@
 <script lang="ts">
     import { selectAPK } from "../logic/selectFile";
+    export let file: string  = '';
 
     let filePromise: Promise<string> | undefined;
+    let rejected: boolean = false;
+
+    function openFilePickingDialog() {
+        rejected = false;
+        filePromise = selectAPK();
+        filePromise.then(
+            (filePath) => {
+                file = filePath.split("/").pop();
+            },
+            () => {
+                rejected = true;
+            }
+        );
+    }
 </script>
 
 <div class="w-[80%] flex flex-row gap-2">
     <div
         class="input input-bordered w-full p-2 flex justify-start items-center hover:bg-base-200"
-        on:click={() => {
-            filePromise = selectAPK();
-        }}
+        class:text-error={rejected}
+        class:border-error={rejected}
+        on:click={openFilePickingDialog}
     >
         <span class="block">
             {#if filePromise}
                 {#await filePromise}
                     Waiting for file selection...
-                {:then filePath}
-                    {filePath.split("/").pop()}
+                {:then _}
+                    {file}
                 {:catch _}
                     Error while selecting file
                 {/await}
