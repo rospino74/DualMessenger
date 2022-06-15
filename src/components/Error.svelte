9ac984ae6acc2a584497e9f46da8e47dc1aedf7c
Marko <34315725+rospino74@users.noreply.github.com>
2022-06-15 17:55:04 +0000

    Added error page

---
+++ src/components/Error.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { _ } from "svelte-i18n";
+    import { LifeBuoyIcon } from "svelte-feather-icons";
+</script>
+
+<div class="box">
+    <LifeBuoyIcon class="error-icon" />
+    <h1>{$_("error_funny")}</h1>
+    <p>
+        <slot />
+    </p>
+</div>
+
+<style lang="scss">
+    .box {
+        @apply flex flex-col justify-center items-center w-full h-full;
+
+        :global(.error-icon) {
+            @apply block stroke-error w-32 h-32 mb-6;
+        }
+
+        h1 {
+            @apply font-bold text-4xl text-error-content mb-4;
+        }
+
+        p {
+            @apply text-2xl italic text-error-content opacity-80;
+        }
+    }
+</style>
